6ca122216b27364d68fb88d592963c675ae173b8
John Kordich <REDMOND\johnkord>
2014-03-25 10:25:22 -0700

    Adding base nxService resource

---
+++ Providers/nxService/MSFT_nxServiceResource.c
@@ -0,0 +1,164 @@
+/* @migen@ */
+#include <MI.h>
+#include "MSFT_nxServiceResource.h"
+
+void MI_CALL MSFT_nxServiceResource_Load(
+    _Outptr_result_maybenull_ MSFT_nxServiceResource_Self** self,
+    _In_opt_ MI_Module_Self* selfModule,
+    _In_ MI_Context* context)
+{
+    MI_UNREFERENCED_PARAMETER(selfModule);
+
+    *self = NULL;
+    MI_Context_PostResult(context, MI_RESULT_OK);
+}
+
+void MI_CALL MSFT_nxServiceResource_Unload(
+    _In_opt_ MSFT_nxServiceResource_Self* self,
+    _In_ MI_Context* context)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+
+    MI_Context_PostResult(context, MI_RESULT_OK);
+}
+
+void MI_CALL MSFT_nxServiceResource_EnumerateInstances(
+    _In_opt_ MSFT_nxServiceResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_opt_ const MI_PropertySet* propertySet,
+    _In_ MI_Boolean keysOnly,
+    _In_opt_ const MI_Filter* filter)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(propertySet);
+    MI_UNREFERENCED_PARAMETER(keysOnly);
+    MI_UNREFERENCED_PARAMETER(filter);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL MSFT_nxServiceResource_GetInstance(
+    _In_opt_ MSFT_nxServiceResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_ const MSFT_nxServiceResource* instanceName,
+    _In_opt_ const MI_PropertySet* propertySet)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+    MI_UNREFERENCED_PARAMETER(propertySet);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL MSFT_nxServiceResource_CreateInstance(
+    _In_opt_ MSFT_nxServiceResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_ const MSFT_nxServiceResource* newInstance)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(newInstance);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL MSFT_nxServiceResource_ModifyInstance(
+    _In_opt_ MSFT_nxServiceResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_ const MSFT_nxServiceResource* modifiedInstance,
+    _In_opt_ const MI_PropertySet* propertySet)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(modifiedInstance);
+    MI_UNREFERENCED_PARAMETER(propertySet);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL MSFT_nxServiceResource_DeleteInstance(
+    _In_opt_ MSFT_nxServiceResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_ const MSFT_nxServiceResource* instanceName)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL MSFT_nxServiceResource_Invoke_GetTargetResource(
+    _In_opt_ MSFT_nxServiceResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_opt_z_ const MI_Char* methodName,
+    _In_ const MSFT_nxServiceResource* instanceName,
+    _In_opt_ const MSFT_nxServiceResource_GetTargetResource* in)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(methodName);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+    MI_UNREFERENCED_PARAMETER(in);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL MSFT_nxServiceResource_Invoke_TestTargetResource(
+    _In_opt_ MSFT_nxServiceResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_opt_z_ const MI_Char* methodName,
+    _In_ const MSFT_nxServiceResource* instanceName,
+    _In_opt_ const MSFT_nxServiceResource_TestTargetResource* in)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(methodName);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+    MI_UNREFERENCED_PARAMETER(in);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
+void MI_CALL MSFT_nxServiceResource_Invoke_SetTargetResource(
+    _In_opt_ MSFT_nxServiceResource_Self* self,
+    _In_ MI_Context* context,
+    _In_opt_z_ const MI_Char* nameSpace,
+    _In_opt_z_ const MI_Char* className,
+    _In_opt_z_ const MI_Char* methodName,
+    _In_ const MSFT_nxServiceResource* instanceName,
+    _In_opt_ const MSFT_nxServiceResource_SetTargetResource* in)
+{
+    MI_UNREFERENCED_PARAMETER(self);
+    MI_UNREFERENCED_PARAMETER(nameSpace);
+    MI_UNREFERENCED_PARAMETER(className);
+    MI_UNREFERENCED_PARAMETER(methodName);
+    MI_UNREFERENCED_PARAMETER(instanceName);
+    MI_UNREFERENCED_PARAMETER(in);
+
+    MI_Context_PostResult(context, MI_RESULT_NOT_SUPPORTED);
+}
+
