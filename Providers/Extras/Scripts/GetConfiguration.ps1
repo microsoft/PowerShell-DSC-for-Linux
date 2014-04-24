aef23193863817177e8076881497bad3340c2a2b
John Kordich <REDMOND\johnkord>
2014-04-24 15:30:15 -0700

    Changes namespace of dsc from "dsc" to "root/Microsoft/DesiredStateConfiguration" with a symlink in place for the Windows namespace "root/Microsoft/Windows/DesiredStateConfiguration"

--- Providers/Extras/Scripts/GetConfiguration.ps1
+++ Providers/Extras/Scripts/GetConfiguration.ps1
@@ -8,5 +8,5 @@ Param(
 $secpass =  ConvertTo-SecureString $Password -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential($Login,$secpass)
 $omiSess = New-CimSession -ComputerName $ComputerName -Authentication Basic -SessionOption (New-CimSessionOption -Protocol Wsman) -Credential $cred -SkipTestConnection
-$r=Invoke-CimMethod -CimSession $omiSess -Namespace "dsc" -ClassName MSFT_DSCLocalConfigurationManager -MethodName GetConfiguration
+$r=Invoke-CimMethod -CimSession $omiSess -Namespace "root/Microsoft/DesiredStateConfiguration" -ClassName MSFT_DSCLocalConfigurationManager -MethodName GetConfiguration
 echo $r.configurations
