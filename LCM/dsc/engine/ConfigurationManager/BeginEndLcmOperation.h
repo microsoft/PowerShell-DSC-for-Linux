9adea2bcfefec7382a8f2d70e13bde4bc2533f76
John Kordich <REDMOND\johnkord>
2014-05-16 14:13:54 -0700

    Changing copyright headers

--- LCM/dsc/engine/ConfigurationManager/BeginEndLcmOperation.h
+++ LCM/dsc/engine/ConfigurationManager/BeginEndLcmOperation.h
@@ -3,7 +3,7 @@
 **
 ** Open Management Infrastructure (OMI)
 **
-** Copyright (c) Microsoft Corporation
+** Copyright (c) Microsoft Corporation. All rights reserved. See license.txt for license information.
 **
 ** Licensed under the Apache License, Version 2.0 (the "License"); you may not
 ** use this file except in compliance with the License. You may obtain a copy
