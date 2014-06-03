dcbb10c5ce93368a376096941a8fe0563e1441d1
Frida Xia (Wicresoft) <FAREAST\v-fridax:1>
2014-06-02 22:06:41 -0700

    1. Realize test code for nxLog automation test. 2. Add all test cases.

---
+++ Test/DSC/DSC/nxLogProviderTest/LogMofHelper.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LogMofHelper.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// <author>v-fridax@microsoft.com</author>
+// <description>Inherit from MofHelper, extend MofTemplate property. </description>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSC
+{
+    class LogMofHelper : MofHelper
+    {
+        public LogMofHelper()
+        {
+            MofGenerator = base.GeneratorFormat.
+                    Replace("$ResourceType", "nxLog").
+                    Replace("$ResourceName", "Log");
+        }
+    }
+
+}
