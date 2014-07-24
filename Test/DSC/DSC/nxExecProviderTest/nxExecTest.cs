95ad23238a246d97c6e6565319476380bee18817
Frida Xia (Wicresoft) <FAREAST\v-fridax:1>
2014-07-23 19:55:06 -0700

    Check in ExecMofHelper.cs and nxExecTest.cs

---
+++ Test/DSC/DSC/nxExecProviderTest/nxExecTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Infra.Frmwrk;
+
+namespace DSC
+{
+    class nxExecTest : ProviderTestBase
+    {
+
+        public override void Setup(IContext ctx)
+        {
+            ctx.Alw("nxExecTest Setup Begin.");
+
+            mofHelper = new EnvironmentMofHelper();
+
+            initializeCmd = ctx.Records.GetValue("initialCmd");
+            finalizeCmd = ctx.Records.GetValue("finalCmd");
+
+            base.Setup(ctx);
+
+            ctx.Alw("nxExecTest Setup End.");
+        }
+
+    }
+}
