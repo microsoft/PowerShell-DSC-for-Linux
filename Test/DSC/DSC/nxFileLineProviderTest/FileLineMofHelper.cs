24332de10f5ea34e415f0b5ca06d43ed9ea5775c
Ting Liu (Wicresoft) <FAREAST\v-litin>
2015-03-05 01:30:08 -0800

    Add nxFileLine Provider test cases.

---
+++ Test/DSC/DSC/nxFileLineProviderTest/FileLineMofHelper.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FileLineMofHelper.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+// <author>v-tilin@microsoft.com</author>
+// <description>Inherit from MofHelper, extend MofTemplate property. </description>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSC
+{
+    class FileLineMofHelper : MofHelper
+    {
+        public FileLineMofHelper()
+        {
+            MofGenerator = base.GeneratorFormat.
+                Replace("$ResourceType", "nxFileLine").
+                Replace("$ResourceName", "FileLine");
+        }
+
+    }
+}
---
+++ Test/DSC/DSC/nxFileLineProviderTest/nxFileLineTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Infra.Frmwrk;
+
+namespace DSC
+{
+    class nxFileLineTest : ProviderTestBase
+    {
+
+        protected SshHelper sshHelper;
+        public override void Setup(IContext ctx)
+        {
+            ctx.Alw("nxFileLineTest Setup Begin.");
+
+            mofHelper = new FileLineMofHelper();
+
+            initializeCmd = ctx.Records.GetValue("initialCmd");
+            finalizeCmd = ctx.Records.GetValue("finalCmd");
+
+            base.Setup(ctx);
+
+            ctx.Alw("nxFileLineTest Setup End.");
+        }
+
+    }
+}
