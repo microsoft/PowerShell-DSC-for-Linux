3ddf583f96dfcdffa7d9d145f1bfc7d725d1cfc0
John Kordich <REDMOND\johnkord>
2015-09-28 14:45:03 -0700

    Fixing status report stuff and updating version number to 1.1.0

--- LCM/scripts/RegisterHelper.sh
+++ LCM/scripts/RegisterHelper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 NodeName=`hostname`
 
-IPAddress=`ip addr | grep 'inet' | awk '{print $2}' | sed 's/\/.*//' | tr "\n" ";"`;
+IPAddress=`ip addr | grep 'inet' | awk '{print $2}' | sed 's/\/.*//' | tr "\n" ";" | cut -c 1-254`;
 
 RegistrationKey="$3"
 
@@ -11,7 +11,6 @@ else
     ConfigurationName=""
 fi
 
-#TODO: change?
 FriendlyName="DSC-OaaS Client Authentication"
 Issuer=`openssl x509 -text -in /etc/opt/omi/ssl/oaas.crt | grep Issuer: | head -n 1 | awk '{print $2}'`
 NotAfter_pre=`openssl x509 -dates -in /etc/opt/omi/ssl/oaas.crt | grep notAfter | sed 's/^.*=//'`
@@ -32,22 +31,11 @@ x_ms_date=`date --utc +%Y-%m-%dT%T.%7NZ | tr -d "\n"`
 
 requestBody="{\"AgentInformation\":{\"LCMVersion\":\"2.0\",\"NodeName\":\"$NodeName\",\"IPAddress\":\"$IPAddress\"},$ConfigurationName\"RegistrationInformation\":{\"CertificateInformation\":{\"FriendlyName\":\"$FriendlyName\",\"Issuer\":\"$Issuer\",\"NotAfter\":\"$NotAfter\",\"NotBefore\":\"$NotBefore\",\"Subject\":\"$Subject\",\"PublicKey\":\"$PublicKey\",\"Thumbprint\":\"$Thumbprint\",\"Version\":3},\"RegistrationMessageType\":\"$RegistrationMessageType\"}}"
 
-# BEGIN TEST CODE
-#testBody='{"AgentInformation":{"LCMVersion":"2.0","NodeName":"DESKTOP-GDU6CNU","IPAddress":"10.30.69.164;127.0.0.1;2001:4898:200:8:4529:f306:651a:d488;2001:4898:200:8:3520:e3f2:542c:cf95;fe80::4529:f306:651a:d488%2;::2000:0:0:0;::1;::2000:0:0:0"},"ConfigurationNames":["FileConfig"],"RegistrationInformation":{"CertificateInformation":{"FriendlyName":"DSC-OaaS Client Authentication","Issuer":"CN=DSC-OaaS","NotAfter":"2016-07-20T08:20:33.0000000-07:00","NotBefore":"2015-07-20T15:10:34.0000000-07:00","Subject":"CN=DSC-OaaS","PublicKey":"U3lzdGVtLlNlY3VyaXR5LkNyeXB0b2dyYXBoeS5YNTA5Q2VydGlmaWNhdGVzLlB1YmxpY0tleQ==","Thumbprint":"9A30D1E57B44DB0D5DE265619278FC67A7FAA3D6","Version":3},"RegistrationMessageType":"ConfigurationRepository"}}'
-
-#requestBody=$testBody
-#x_ms_date="2015-07-20T22:20:38.3739827Z"
-# END TEST CODE
-
 contentHash=`echo -n "$requestBody" | openssl dgst -binary -sha256 | openssl enc -base64 | tr -d "\n"`
 
 stringToSign=`printf "${contentHash}\n${x_ms_date}"`
 
-# TODO: replace testkey with actual registration key
 signature=`echo -n "$stringToSign" | openssl dgst -binary -sha256 -hmac "$RegistrationKey" | openssl enc -base64 | tr -d "\n"`
-
-
-
 
 if [ "$1" = "body" ]; then
     printf "$requestBody"
--- LCM/scripts/StatusReport.sh
+++ LCM/scripts/StatusReport.sh
@@ -11,15 +11,19 @@ if [ "$2" = "EndTime" ]; then
     if [ -n "$3" ]; then
 	ErrorMessage="\"{\\\"ErrorMessage\\\":\\\"$3\\\"}\""
 	Errors=",\\\"Error\\\":\\\"$3\\\""
+	if [ -n "${14}" ]; then
+	    RNIDS="{\\\"SourceInfo\\\":\\\"$4\\\",\\\"ModuleName\\\":\\\"$5\\\",\\\"DurationInSeconds\\\":\\\"0\\\",\\\"InstanceName\\\":\\\"$7\\\",\\\"StartDate\\\":\\\"$curDate\\\",\\\"ResourceName\\\":\\\"$9\\\",\\\"ModuleVersion\\\":\\\"${10}\\\",\\\"RebootRequested\\\":\\\"${11}\\\",\\\"ResourceId\\\":\\\"${12}\\\",\\\"ConfigurationName\\\":\\\"${13}\\\",\\\"InDesiredState\\\":\\\"${14}\\\"}"
+	else
+	    ErrorMessage="\"{\\\"Locale\\\":\\\"en-US\\\",\\\"ErrorCode\\\":\\\"1\\\",\\\"ErrorMessage\\\":\\\"$3\\\",\\\"ResourceId\\\":\\\"DSCEngine\\\",\\\"ErrorSource\\\":\\\"DSCEngine\\\"}\""
+	fi
+	output="{\"JobId\":\"$1\",\"ConfigurationVersion\":\"2.0.0\",\"ReportFormatVersion\":\"2.0\",\"LCMVersion\":\"2.0\",\"EndTime\":\"$curDate\",\"Errors\":[$ErrorMessage],\"StatusData\":[\"{\\\"Locale\\\":\\\"en-US\\\",\\\"ResourcesNotInDesiredState\\\":[$RNIDS]$Errors}\"]}"
+    else
+	output="{\"JobId\":\"$1\",\"ConfigurationVersion\":\"2.0.0\",\"ReportFormatVersion\":\"2.0\",\"LCMVersion\":\"2.0\",\"EndTime\":\"$curDate\",\"Errors\":[$ErrorMessage],\"StatusData\":[\"{\\\"Locale\\\":\\\"en-US\\\"}\"]}"
     fi
-    if [ -n "${14}" ]; then
-	RNIDS=",\\\"ResourcesNotInDesiredState\\\":[{\\\"SourceInfo\\\":\\\"$4\\\",\\\"ModuleName\\\":\\\"$5\\\",\\\"DurationInSeconds\\\":\\\"0\\\",\\\"InstanceName\\\":\\\"$7\\\",\\\"StartDate\\\":\\\"$curDate\\\",\\\"ResourceName\\\":\\\"$9\\\",\\\"ModuleVersion\\\":\\\"${10}\\\",\\\"RebootRequested\\\":\\\"${11}\\\",\\\"ResourceId\\\":\\\"${12}\\\",\\\"ConfigurationName\\\":\\\"${13}\\\",\\\"InDesiredState\\\":\\\"${14}\\\"}]"
-    fi
-    output="{\"JobId\":\"$1\",\"ConfigurationVersion\":\"2.0.0\",\"LCMVersion\":\"2.0\",\"EndTime\":\"$curDate\",\"Errors\":[$ErrorMessage],\"StatusData\":[\"{\\\"Locale\\\":\\\"en-US\\\"$RNIDS$Errors}\"]}"
 else
-    IPAddress=`ip addr | grep 'inet' | awk '{print $2}' | sed 's/\/.*//' | tr "\n" ";"`
+    IPAddress=`ip addr | grep 'inet' | awk '{print $2}' | sed 's/\/.*//' | tr "\n" ";" | cut -c 1-254`
     NodeName=`hostname`
-    output="{\"JobId\":\"$1\",\"OperationType\":\"Consistency\",\"NodeName\":\"$NodeName\",\"IpAddress\":\"$IPAddress\",\"LCMVersion\":\"2.0\",$RNIDS\"ReportFormatVersion\":\"1.0\",\"StartTime\":\"$curDate\",\"Errors\":[],\"StatusData\":[\"{\\\"Locale\\\":\\\"en-US\\\"}\"]}"
+    output="{\"JobId\":\"$1\",\"OperationType\":\"Consistency\",\"NodeName\":\"$NodeName\",\"IpAddress\":\"$IPAddress\",\"ReportFormatVersion\":\"2.0\",\"LCMVersion\":\"2.0\",\"StartTime\":\"$curDate\",\"Errors\":[],\"StatusData\":[\"{\\\"Locale\\\":\\\"en-US\\\"}\"]}"
 
 fi
 
