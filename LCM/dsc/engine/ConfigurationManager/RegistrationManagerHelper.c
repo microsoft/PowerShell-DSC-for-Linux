f4f29050dfc9423bda5e94830628a303e4ddce4e
John Kordich <REDMOND\johnkord>
2015-09-29 12:36:33 -0700

    Setting permissions on OaaS private key to be root read/write only.

--- LCM/dsc/engine/ConfigurationManager/RegistrationManagerHelper.c
+++ LCM/dsc/engine/ConfigurationManager/RegistrationManagerHelper.c
@@ -104,18 +104,20 @@ MI_Result Register(
     result = GetThumbprintForRegisteredServerURL(self, request->registrationData, &thumbprint, cimErrorDetails);
     if (thumbprint == NULL)
     {
+        system("touch "  CONFIG_CERTSDIR "/oaas.key; chmod 0600 "  CONFIG_CERTSDIR "/oaas.key");
+        system("touch "  CONFIG_CERTSDIR "/oaas.key_old; chmod 0600 "  CONFIG_CERTSDIR "/oaas.key_old");
         systemResult = system("openssl req -subj '/CN=DSC-OaaS' -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout " CONFIG_CERTSDIR "/oaas.key_old -out " CONFIG_CERTSDIR "/oaas.crt && openssl rsa -in " CONFIG_CERTSDIR "/oaas.key_old -out " CONFIG_CERTSDIR "/oaas.key && rm -f " CONFIG_CERTSDIR "/oaas.key_old");
         if (systemResult != 0 && errno != 10)
         {
             DSC_EventWriteLCMServerRegCertGenFailed(g_ConfigurationDetails.jobGuidString, self->agentId);
-	    return GetCimMIError(MI_RESULT_FAILED, cimErrorDetails, ID_PULL_FAILEDTOGENERATECERT);
+            return GetCimMIError(MI_RESULT_FAILED, cimErrorDetails, ID_PULL_FAILEDTOGENERATECERT);
         }
         
         systemResult = system("openssl x509 -noout -in " CONFIG_CERTSDIR "/oaas.crt -fingerprint | sed 's/^.*=//' > " CONFIG_CERTSDIR "/oaas.thumbprint");
         if (systemResult != 0 && errno != 10)
         {
             DSC_EventWriteLCMServerRegCertGenFailed(g_ConfigurationDetails.jobGuidString, self->agentId);
-	    return GetCimMIError(MI_RESULT_FAILED, cimErrorDetails, ID_PULL_FAILEDTOGENERATECERT);
+            return GetCimMIError(MI_RESULT_FAILED, cimErrorDetails, ID_PULL_FAILEDTOGENERATECERT);
         }
         
         {
