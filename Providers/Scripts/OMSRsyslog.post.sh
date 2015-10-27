c3a0d888d63bfd85da272d76aeebb68a86c8a640
Eric Gable (Insight Global) <REDMOND\v-ericg>
2015-10-27 10:23:39 -0700

    nxOMSSyslog.py - Return goodness for sysklogd. Use /etc/rsyslog.conf if /etc/rsyslog.d/ not present. Make #facility optional in UpdateSyslogConf. OMSRsyslog.post.sh - add arg to cp to /etc/rsyslog.conf.

--- Providers/Scripts/OMSRsyslog.post.sh
+++ Providers/Scripts/OMSRsyslog.post.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cp --force /etc/opt/omi/conf/omsconfig/rsyslog-oms.conf /etc/rsyslog.d/95-omsagent.conf
+if [ $1="1" ]; then
+    cp --force /etc/opt/omi/conf/omsconfig/rsyslog-oms.conf /etc/rsyslog.conf
+else
+    cp --force /etc/opt/omi/conf/omsconfig/rsyslog-oms.conf /etc/rsyslog.d/95-omsagent.conf
+fi
 service rsyslog restart
 
