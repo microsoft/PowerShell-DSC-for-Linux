342609e9cf164624a5077fa0f6d2ebbd67f5b4de
Eric Gable (Insight Global) <REDMOND\v-ericg>
2015-10-28 14:03:41 -0700

    nxOMSSyslog.py - nxOMAgent.py - Add 3.x versions

---
+++ Providers/Scripts/3.x/Scripts/nxOMSAgent.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+#============================================================================
+# Copyright (C) Microsoft Corporation, All rights reserved. 
+#============================================================================
+
+import os
+import imp
+import re
+import codecs
+import json
+import functools
+protocol = imp.load_source('protocol', '../protocol.py')
+nxDSCLog = imp.load_source('nxDSCLog', '../nxDSCLog.py')
+
+LG = nxDSCLog.DSCLog
+
+conf_path = '/etc/opt/microsoft/omsagent/conf/omsagent.conf'
+omi_map=None
+
+def init_vars(HeartbeatIntervalSeconds, PerfObject):
+    init_omi_map()
+    if PerfObject is not None :
+        for perf in PerfObject:
+            new_perfs=[]
+            if len(perf['PerformanceCounter'].value):
+                for perf_counter in perf['PerformanceCounter'].value:
+                    new_perfs.append(perf_counter)
+                perf['PerformanceCounter']=new_perfs
+            if perf['InstanceName'].value is None:
+                perf['InstanceName']=''
+            else:
+                perf['InstanceName']=perf['InstanceName'].value
+            if perf['AllInstances'].value is None:
+                perf['AllInstances']=False
+            else:
+                perf['AllInstances']=perf['AllInstances'].value.value
+            perf['IntervalSeconds'] = perf['IntervalSeconds'].value.value
+    
+def Set_Marshall(HeartbeatIntervalSeconds, PerfObject):
+    init_vars(HeartbeatIntervalSeconds, PerfObject)
+    Set(HeartbeatIntervalSeconds, PerfObject)
+    return [0]
+
+def Test_Marshall(HeartbeatIntervalSeconds, PerfObject):
+    init_vars(HeartbeatIntervalSeconds, PerfObject)
+    return Test(HeartbeatIntervalSeconds, PerfObject)
+
+def Get_Marshall(HeartbeatIntervalSeconds, PerfObject):
+    arg_names = list(locals().keys())
+    init_vars(HeartbeatIntervalSeconds, PerfObject)
+    retval = 0
+    NewHeartbeatIntervalSeconds, NewPerf = Get(HeartbeatIntervalSeconds, PerfObject)
+    for perf in NewPerf:
+        if len(perf['PerformanceCounter']):
+            perf['PerformanceCounter'] = protocol.MI_StringA(perf['PerformanceCounter'])
+        perf['InstanceName']=protocol.MI_String(perf['InstanceName'])
+        perf['AllInstances']=protocol.MI_Boolean(perf['AllInstances'])
+        perf['IntervalSeconds']=protocol.MI_Uint16(perf['IntervalSeconds'])
+    PerfObject=protocol.MI_InstanceA(NewPerf)
+    HeartbeatIntervalSeconds=protocol.MI_Uint16(NewHeartbeatIntervalSeconds)
+    retd = {}
+    ld = locals()
+    for k in arg_names:
+        retd[k] = ld[k]
+    return retval, retd
+            
+def Set(HeartbeatIntervalSeconds, PerfObject):
+    if Test(HeartbeatIntervalSeconds, PerfObject) == [0]:
+        return [0]
+    UpdateOMSAgentConf(HeartbeatIntervalSeconds, PerfObject)
+    return [0]
+
+def Test(HeartbeatIntervalSeconds, PerfObject):
+    NewHeartbeatIntervalSeconds, NewPerfs = ReadOMSAgentConf(HeartbeatIntervalSeconds, PerfObject)
+    if NewHeartbeatIntervalSeconds != HeartbeatIntervalSeconds:
+        return [-1]
+    for perf in PerfObject:
+        perf['PerformanceCounter'].sort()
+        found = False
+        for new_perf in NewPerfs:
+            new_perf['PerformanceCounter'].sort()
+            for k in perf.keys():
+                if k in new_perf.keys():
+                    if perf[k] == new_perf[k]:
+                        found = True
+            if found:
+                break
+        if not found:
+            return [-1]
+    return [0]
+
+def Get(HeartbeatIntervalSeconds, PerfObject):
+    NewHeartbeatIntervalSeconds, NewPerf = ReadOMSAgentConf(HeartbeatIntervalSeconds, PerfObject)
+    return NewHeartbeatIntervalSeconds,NewPerf
+
+def TranslatePerfs(perfs):
+    d={}
+    for p in perfs:
+        for cname in omi_map:
+            for prop in cname['CimProperties']:
+                if p == prop['CounterName'] or p == prop['CimPropertyName'] :
+                    if cname['ObjectName'] not in d.keys():
+                        d[cname['ObjectName']]=[p]
+                    else:
+                        d[cname['ObjectName']].append(p)
+    return d
+
+def ReadOMSAgentConf(HeartbeatIntervalSeconds,PerfObject):
+    txt = codecs.open(conf_path, 'r', 'utf8').read()
+    heartbeat_srch_str=r'<source>.*?tag heartbeat.*?run_interval ([0-9]+[a-z])\n</source>\n'
+    heartbeat_srch=re.compile(heartbeat_srch_str,re.M|re.S)
+    m=heartbeat_srch.search(txt)
+    if m is not None:
+        interval=int(m.group(1)[:-1])
+        if m.group(1)[-1:] == 'm':
+            interval*= 60
+    else :
+        interval=None
+    new_heartbeat = interval
+    perf_src_srch_str=r'\n<source>\n  type oms_omi.*?instance_regex "(.*?)".*?counter_name_regex "(.*?)".*?interval ([0-9]+[a-z]).*?</source>\n'
+    perf_src_srch=re.compile(perf_src_srch_str,re.M|re.S)
+    new_perfobj=[]
+    sources=perf_src_srch.findall(txt)
+    inst=''
+    interval=0
+    all_instances=False
+    for perf in PerfObject:
+        perflist=[]
+        for source in sources:
+            for p in perf['PerformanceCounter']:
+                if p not in perflist and p in source[1]:
+                    perflist.append(p)
+                    interval=int(source[2][:-1])
+                    if source[2][-1:] == 'm':
+                        interval*= 60
+                    inst=source[0]
+        if len(perflist) > 0:
+            if '.*' not in perf['InstanceName']:
+                inst=inst.replace('.*','*')
+            all_instances=False
+            if inst == '*':
+                all_instances=True
+            new_perfobj.append({'PerformanceCounter':perflist,'InstanceName':inst,'IntervalSeconds':interval,'AllInstances':all_instances})
+    return new_heartbeat,new_perfobj
+            
+    
+def UpdateOMSAgentConf(HeartbeatIntervalSeconds,PerfObject):
+    if os.path.exists(conf_path):
+        txt = codecs.open(conf_path, 'r', 'utf8').read()
+    else:
+        txt=''
+    heartbeat_srch_str=r'<source>.*?tag heartbeat.*?</source>\n'
+    heartbeat_srch=re.compile(heartbeat_srch_str,re.M|re.S)
+    heartbeat_src='<source>\n  type exec\n  tag heartbeat.output\n  command /opt/microsoft/omsagent/bin/omsadmin.sh -b\n  format tsv\n  keys severity,message\n  run_interval ' + str(HeartbeatIntervalSeconds) + 's\n</source>\n'
+    txt=heartbeat_srch.sub(heartbeat_src,txt)
+    d={}
+    perf_src_srch_str=r'\n<source>\n  type oms_omi.*?</source>\n'
+    perf_src_srch=re.compile(perf_src_srch_str,re.M|re.S)
+    for source in perf_src_srch.findall(txt):
+        txt=txt.replace(source,'')
+    new_source=''
+    for perf in PerfObject:
+        d=TranslatePerfs(perf['PerformanceCounter'])
+        for k in d.keys():
+            names='('+functools.reduce(lambda x, y: x + '|' + y,d[k])+')'
+            instances=re.sub(r'([><]|&gt|&lt)','',perf['InstanceName'])
+            instances=re.sub(r'([*])','.*',instances)
+            new_source+='\n<source>\n  type oms_omi\n  object_name "'+k+'"\n  instance_regex "' + instances + '"\n  counter_name_regex "'+ names +'"\n  interval '+str(perf['IntervalSeconds'])+'s\n</source>\n'
+    m=heartbeat_srch.search(txt)
+    i=m.end(0)+1
+    txt=txt[:i]+new_source+txt[i:]
+    codecs.open(conf_path, 'w', 'utf8').write(txt)
+    os.system('sudo /opt/microsoft/omsagent/bin/service_control restart')
+
+def init_omi_map():
+    global omi_map
+    txt=codecs.open('/etc/opt/microsoft/omsagent/sysconf/omi_mapping.json', 'r', 'utf8').read()
+    omi_map=json.loads(txt)
+
---
+++ Providers/Scripts/3.x/Scripts/nxOMSSyslog.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+#============================================================================
+# Copyright (C) Microsoft Corporation, All rights reserved. 
+#============================================================================
+
+import os
+import imp
+import re
+import codecs
+protocol = imp.load_source('protocol', '../protocol.py')
+nxDSCLog = imp.load_source('nxDSCLog', '../nxDSCLog.py')
+
+LG = nxDSCLog.DSCLog
+
+rsyslog_conf_path='/etc/rsyslog.conf'
+syslog_ng_conf_path='/etc/syslog-ng/syslog-ng.conf'
+sysklog_conf_path='/etc/syslog.conf'
+oms_syslog_ng_conf_path = '/etc/opt/omi/conf/omsconfig/syslog-ng-oms.conf'
+oms_rsyslog_conf_path = '/etc/opt/omi/conf/omsconfig/rsyslog-oms.conf'
+oms_sysklog_conf_path = '/etc/opt/omi/conf/omsconfig/sysklog-oms.conf'
+conf_path=''
+
+def init_vars(SyslogSource):
+    global conf_path
+    for source in SyslogSource:
+        if source['Severities'] is not None:
+            if 'value' in dir(source['Severities']):
+                source['Severities']=source['Severities'].value
+        if 'value' in dir(source['Facility']):
+            source['Facility']=source['Facility'].value
+    if os.path.exists(rsyslog_conf_path):
+        conf_path=oms_rsyslog_conf_path
+    elif os.path.exists(syslog_ng_conf_path):
+        conf_path=oms_syslog_ng_conf_path
+    elif os.path.exists(sysklog_conf_path):
+        conf_path=oms_sysklog_conf_path
+    else:
+        LG().Log('ERROR', 'Unable to find OMS config files')
+        raise Exception('Unable to find OMS config files')
+    
+def Set_Marshall(SyslogSource):
+    init_vars(SyslogSource)
+    retval = Set(SyslogSource)
+    if retval is False:
+        retval = [-1]
+    else:
+        retval = [0]
+    return retval
+
+def Test_Marshall(SyslogSource):
+    init_vars(SyslogSource)
+    return Test(SyslogSource)
+
+def Get_Marshall(SyslogSource):
+    arg_names = list(locals().keys())
+    init_vars(SyslogSource)
+    retval = 0
+    NewSource = Get(SyslogSource)
+    for source in NewSource:
+        if source['Severities'] is not None:
+            source['Severities'] = protocol.MI_StringA(source['Severities'])
+        source['Facility']=protocol.MI_String(source['Facility'])
+    SyslogSource=protocol.MI_InstanceA(NewSource)
+    retd = {}
+    ld = locals()
+    for k in arg_names:
+        retd[k] = ld[k]
+    return retval, retd
+            
+def Set(SyslogSource):
+    if Test(SyslogSource) == [0]:
+        return [0]
+    if conf_path==oms_syslog_ng_conf_path:
+        ret=UpdateSyslogNGConf(SyslogSource)
+    else:
+        ret=UpdateSyslogConf(SyslogSource)
+    if ret:
+        ret = [0]
+    else:
+        ret = [-1]
+    return ret
+
+def Test(SyslogSource):
+    if conf_path==oms_sysklog_conf_path:
+        return [0]
+    if conf_path==oms_syslog_ng_conf_path:
+        NewSource=ReadSyslogNGConf(SyslogSource)
+    else:
+        NewSource=ReadSyslogConf(SyslogSource)
+    for d in SyslogSource:
+        found = False
+        if 'Severities' not in d.keys() or d['Severities'] is None or len(d['Severities']) is 0:
+            d['Severities']=['none'] # redundant?
+        d['Severities'].sort()
+        for n in NewSource:
+            n['Severities'].sort()
+            if d['Facility'] == n['Facility'] and d['Severities'] == n['Severities']:
+                found = True
+        if not found:
+            return [-1]
+    return [0]
+
+def Get(SyslogSource):
+    if conf_path==oms_sysklog_conf_path:
+        return SyslogSource
+    if conf_path==oms_syslog_ng_conf_path:
+        NewSource=ReadSyslogNGConf(SyslogSource)
+    else:
+        NewSource=ReadSyslogConf(SyslogSource)
+    for d in NewSource:
+        if d['Severities'] == ['none']:
+            d['Severities'] = []
+    return NewSource
+
+def ReadSyslogConf(SyslogSource):
+    out = []
+    if len(SyslogSource) == 0:
+        return out
+    if not os.path.exists('/etc/rsyslog.d'):
+        txt = codecs.open(rsyslog_conf_path, 'r', 'utf8').read()
+    else:
+        if not os.path.exists(conf_path):
+            LG().Log('ERROR', 'Unable to read ' + conf_path + '.')
+            return out
+        txt = codecs.open(conf_path, 'r', 'utf8').read()
+    count=0
+    for d in SyslogSource:
+        out.append({'Facility':d['Facility'],'Severities':[]})
+        facility_search = r'(?P<facility>'+d['Facility']+r')(?P<op>[.]=|[.])(?P<severity>.*?)[ ;\t]'
+        facility_re=re.compile(facility_search,re.M|re.S)
+        s=facility_re.findall(txt)
+        if 'Severities' not in d.keys() or d['Severities'] is None or len(d['Severities']) is 0:
+            d['Severities']=['none']
+        for m in s:
+            if m[2].encode('ascii','ignore') in d['Severities']:
+                (out[count])['Severities'].append(m[2].encode('ascii','ignore'))
+        count+=1
+    return out
+
+def UpdateSyslogConf(SyslogSource):
+    arg=''
+    if 'rsyslog' in conf_path :
+        if os.path.exists('/etc/rsyslog.d'):
+            txt = ''
+        elif os.path.exists(rsyslog_conf_path):    
+            txt = codecs.open(rsyslog_conf_path, 'r', 'utf8').read()
+            arg = '1'
+        else :
+            LG().Log('ERROR', 'Unable to read ' + rsyslog_conf_path + '.')
+    elif not os.path.exists(conf_path):
+        LG().Log('ERROR', 'Unable to read ' + conf_path + '.')
+        return False
+#     else :
+#         txt = codecs.open(sysklog_conf_path, 'r', 'utf8').read()
+    facility_search = r'(#facility.*?\n.*?25224\n)|(^[^#].*?25224\n)'
+    facility_re=re.compile(facility_search,re.M|re.S)
+    for t in facility_re.findall(txt):
+        for r in t:
+            txt=txt.replace(r,'')
+    for d in SyslogSource:
+        facility_txt = '#facility = ' + d['Facility'] + '\n'
+        for s in d['Severities']:
+            facility_txt += d['Facility'] + '.=' + s + ';'
+        facility_txt=facility_txt[0:-1]+'\t@127.0.0.1:25224\n'
+        txt += facility_txt
+    codecs.open(conf_path, 'w', 'utf8').write(txt)
+    if 'rsyslog' in conf_path:
+        os.system('sudo /opt/microsoft/omsconfig/Scripts/OMSRsyslog.post.sh '+arg)
+#     else: # sysklogd
+#         os.system('sudo /opt/microsoft/omsconfig/Scripts/OMSSysklog.post.sh')
+    return True
+    
+def ReadSyslogNGConf(SyslogSource):
+    out = []
+    if not os.path.exists(syslog_ng_conf_path):
+        LG().Log('ERROR', 'Unable to read ' + syslog_ng_conf_path + '.')
+        return out
+    txt = codecs.open(syslog_ng_conf_path, 'r', 'utf8').read()
+    count=0
+    for d in SyslogSource:
+        out.append({'Facility':d['Facility'],'Severities':[]})
+        facility_search = r'filter f_'+d['Facility']+r'_oms.*?level[(](?P<severities>.*?)[)]'
+        facility_re=re.compile(facility_search,re.M|re.S)
+        s=facility_re.search(txt)
+        sevs=[]
+        if s is not None:
+            sevs=s.group('severities').encode('ascii','ignore').split(',')
+        if 'Severities' not in d.keys() or d['Severities'] is None or len(d['Severities']) is 0:
+            d['Severities']=['none']
+        for m in sevs:
+            if m in d['Severities']:
+                (out[count])['Severities'].append(m)
+        count+=1
+    return out
+    
+def UpdateSyslogNGConf(SyslogSource):
+    if not os.path.exists(syslog_ng_conf_path):
+        LG().Log('ERROR', 'Unable to read ' + conf_path + '.')
+        return False
+    txt = codecs.open(syslog_ng_conf_path, 'r', 'utf8').read()
+    facility_search = r'(\n+)?(#OMS_Destination.*?25224.*?\n)?(\n)?(#OMS_facility.*?filter.*?_oms.*?log.*destination.*?\n)'
+    facility_re = re.compile(facility_search,re.M|re.S)
+    txt = facility_re.sub('',txt)
+    txt += '\n\n#OMS_Destination\ndestination d_oms { udp("127.0.0.1" port(25224)); };\n'
+    for d in SyslogSource:
+        if 'Severities' not in d.keys() or d['Severities'] is None or len(d['Severities']) is 0:
+            facility_txt = ''
+        else :
+            facility_txt = '\n#OMS_facility = ' + d['Facility'] + '\n'
+            sevs=reduce(lambda x, y: x + ',' + y,d['Severities'])
+            facility_txt += 'filter f_' + d['Facility'] + '_oms { level('+ sevs +') and facility(' + d['Facility'] + '); };\n'
+            facility_txt += 'log { source(src); filter(f_' + d['Facility'] + '_oms); destination(d_oms); };\n'
+            txt += facility_txt
+    codecs.open(conf_path, 'w', 'utf8').write(txt)
+    os.system('sudo /opt/microsoft/omsconfig/Scripts/OMSSyslog-ng.post.sh')
+    return True
