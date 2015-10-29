8b72eed0f985005f3907243a2b737e15e13b09ed
Kristopher Bash <REDMOND\krbash>
2015-10-29 09:58:16 -0700

    Fix if clause for OMSRsyslog.post.sh

--- Providers/Scripts/OMSRsyslog.post.sh
+++ Providers/Scripts/OMSRsyslog.post.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $1="1" ]; then
+if [ "$1" = "1" ]; then
     cp --force /etc/opt/omi/conf/omsconfig/rsyslog-oms.conf /etc/rsyslog.conf
 else
     cp --force /etc/opt/omi/conf/omsconfig/rsyslog-oms.conf /etc/rsyslog.d/95-omsagent.conf
