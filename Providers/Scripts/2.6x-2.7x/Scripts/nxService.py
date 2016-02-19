14017f5acbed84fb489709a395fab33f5b315208
John Kordich <johnkord@microsoft.com>
2016-02-19 10:26:30 -0800

    Added '__Inventory' dictionaries to return values for inventory methods in python

--- Providers/Scripts/2.6x-2.7x/Scripts/nxService.py
+++ Providers/Scripts/2.6x-2.7x/Scripts/nxService.py
@@ -107,6 +107,8 @@ def Inventory_Marshall(Name, Controller, Enabled, State):
         srv['Description'] = protocol.MI_String(srv['Description'])
         srv['Runlevels'] = protocol.MI_String(srv['Runlevels'])
     Inventory=protocol.MI_InstanceA(sc.services_list)
+    retd = {}
+    retd["__Inventory"] = Inventory
     return 0, Inventory
 
 # ##########################
