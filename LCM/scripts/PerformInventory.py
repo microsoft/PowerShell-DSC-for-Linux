af714b3d7bb7dd123a8faddead065d15908db41a
John Kordich <johnkord@microsoft.com>
2016-02-24 10:49:22 -0800

    Adding PerformInventory.py

---
+++ LCM/scripts/PerformInventory.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import fileinput
+import sys
+import subprocess
+
+omi_bindir = "<CONFIG_BINDIR>"
+omicli_path = omi_bindir + "/omicli"
+
+parameters = []
+parameters.append(omicli_path)
+parameters.append("iv")
+parameters.append("<DSC_NAMESPACE>")
+parameters.append("{")
+parameters.append("MSFT_DSCLocalConfigurationManager")
+parameters.append("}")
+parameters.append("PerformInventory")
+
+#s = ""
+#for param in parameters:
+#    s += param + " "
+
+p = subprocess.Popen(parameters, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout, stderr = p.communicate()
+
+print(stdout)
+print(stderr)
+
+
