b7f239e7ed2a9520576465b44922960b0d83f7de
John Kordich <johnkord@microsoft.com>
2016-04-07 13:12:19 -0700

    Allowing ImportGPGKey.sh to write to a specific keyring

--- LCM/scripts/ImportGPGKey.sh
+++ LCM/scripts/ImportGPGKey.sh
@@ -6,14 +6,20 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+if [ -z "$2" ]; then
+    KEYRING_NAME="keyring.gpg"
+else
+    KEYRING_NAME=$2
+fi
+
 DSC_ETC_DIR=<CONFIG_SYSCONFDIR>/<CONFIG_SYSCONFDIR_DSC>
-HOME=$DSC_ETC_DIR gpg --no-default-keyring  --keyring $DSC_ETC_DIR/keyring.gpg --import $1
+HOME=$DSC_ETC_DIR gpg --no-default-keyring  --keyring $DSC_ETC_DIR/$KEYRING_NAME --import $1
 RETVAL=$?
 
 if [ "<CONFIG_SYSCONFDIR_DSC>" = "omsagent" ]; then
-    chown omsagent $DSC_ETC_DIR/keyring.gpg
+    chown omsagent $DSC_ETC_DIR/$KEYRING_NAME
 else
-    chown root $DSC_ETC_DIR/keyring.gpg
+    chown root $DSC_ETC_DIR/$KEYRING_NAME
 fi
 
 exit $RETVAL
