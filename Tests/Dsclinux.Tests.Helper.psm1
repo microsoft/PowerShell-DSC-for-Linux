fbd33c1b21f1b31709625aa09fbeef0e1091de68
Fayza Sultan <fayzas@microsoft.com>
2017-02-01 15:48:25 -0800

    Addressed CR Comments.

--- Tests/Dsclinux.Tests.Helper.psm1
+++ Tests/Dsclinux.Tests.Helper.psm1
@@ -1,37 +1,50 @@
-#
-# Create a connection to the OMI server: 
-#
+
+<#
+.SYNOPSIS
+    Creates a connection to the OMI server
+#>
 function CreateCimSession
 
 {
-    param 
-    ( 
-        [Parameter(Mandatory=$true)] 
-        [PSCredential] $credential, 
- 
-        [Parameter(Mandatory=$true)] 
-        [ValidateNotNullOrEmpty()] 
-        [string] $server, 
- 
-        [ValidateNotNull()] 
-        [Uint32] $port = 5986 
- 
-    ) 
- 
-    $sessionOptions = New-CimSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck -UseSsl 
-    $session = New-CimSession -Credential $credential -ComputerName $server -Port $port -Authentication Basic -SessionOption $sessionOptions -OperationTimeoutSec:30 
-    return $session 
- 
+    param
+    (
+     [Parameter(Mandatory=$true)]
+     [PSCredential] $credential,
+
+     [Parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]
+     [string] $server,
+
+     [ValidateNotNull()]
+     [Uint32] $port = 5986
+
+    )
+
+    $sessionOptions = New-CimSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck -UseSsl
+    $session = New-CimSession -Credential $credential -ComputerName $server -Port $port -Authentication Basic -SessionOption $sessionOptions -OperationTimeoutSec:30
+    return $session
+
 }
 
-#
-# Get Linux Client Information 
-#
+<#
+.SYNOPSIS
+    Gets information about test environment for DSC Linux
+
+.DESCRIPTION
+    Reads Information about DSC Linux client & Pull Server from JSON file. 
+#>
 function ImportLinuxTestConfigurtaion
 
 {
-    $testMatrix = Get-Content .\DSCLinuxTestConfig.json | ConvertFrom-Json            
-    return $testMatrix[0].LinuxClient
+     param
+    (     
+     [Parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]
+     [string] $testConfigFile
+   )
+
+    $testMatrix = Get-Content $testConfigFile | ConvertFrom-Json            
+    return $testMatrix[0]
 }
 
 Export-ModuleMember -function ImportLinuxTestConfigurtaion, CreateCimSession
