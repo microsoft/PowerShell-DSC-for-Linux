e34b4869ce7c6ab10e841cbae215c25d60717aca
Atchut Barli <atchub@microsoft.com>
2017-06-22 14:00:52 -0700

    performance improvement for YUM update assessment

--- Providers/Scripts/OMSYumUpdates.sh
+++ Providers/Scripts/OMSYumUpdates.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [ "$1" = "" ]; then
-    yum check-update
+    yum check-update |  awk '{print $1}' 
+    exit ${PIPESTATUS[0]}   
 else
     if [ -e "/usr/bin/repoquery" ]; then
-	repoquery --queryformat "Name : %{NAME}\nArch : %{ARCH}\nVersion :  %{EPOCH}:%{VERSION}\nRelease : %{RELEASE}\nRepo : %{REPO}\nBuildtime : %{BUILDTIME}" $1
+	    repoquery --queryformat "Name : %{NAME}\nArch : %{ARCH}\nVersion :  %{EPOCH}:%{VERSION}\nRelease : %{RELEASE}\nRepo : %{REPO}\nBuildtime : %{BUILDTIME}" $*
     else
-	yum info available $1
+	    yum info available $*
     fi
 fi
