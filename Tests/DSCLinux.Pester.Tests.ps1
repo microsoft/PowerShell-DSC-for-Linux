5dbeb6b3c5b7294017f36fb7cc21cc7da08cf58f
Katie Keim <kakeim@microsoft.com>
2017-08-18 12:42:13 -0700

    Update regression test file

--- Tests/DSCLinux.Pester.Tests.ps1
+++ Tests/DSCLinux.Pester.Tests.ps1
@@ -4,13 +4,10 @@
 # Copyright (c) Microsoft Corporation, 2017
 #
 
-
-Import-Module ".\Dsclinux.Tests.Helper.psm1" -force -DisableNameChecking
+Import-Module ".\Dsclinux.Tests.Helper.psm1" -Force -DisableNameChecking
 
 Describe "DSC Linux Sanity Tests" -Tags @('Feature') {
-
     BeforeAll {               
-
         #Install Modules needed for compliation on Windows machine 
         Install-Module nx, nxComputerManagement, nxNetworking
 
@@ -21,81 +18,61 @@ Describe "DSC Linux Sanity Tests" -Tags @('Feature') {
         $rootUser = $cfg.LinuxClient.UserName
         $rootPassword = $cfg.LinuxClient.Password
 
-
         $rootUserPassword = ConvertTo-SecureString -String $rootPassword -AsPlainText -Force
         $rootCredentials = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $rootUser, $rootUserPassword
-        $script:session  = CreateCimSession -credential $rootCredentials  -server $linuxClientName         
-    
+        $script:session  = CreateCimSession -credential $rootCredentials  -server $linuxClientName           
     }
 
-    AfterAll {      
-    }
-    
-    BeforeEach {
-      
-    }
-
-    AfterEach {
-      
-    }
-	
-	It "Verify we can Set and get Meta Configuration" {
-            
-           & "$PWD\Configurations\MetaConfigPush.ps1" -targetClient $script:linuxClientName 
-           Set-DscLocalConfigurationManager -Path "$env:temp\MetaConfigPush"  -Verbose -CimSession $script:session
-           
-           $metaConfig = Get-DscLocalConfigurationManager -Verbose -CimSession $script:session
-           $metaConfig.RefreshMode | Should Be "Push" 
-           $metaConfig.ConfigurationModeFrequencyMins | Should Be "60"           
+	It "Verify we can Set and get Meta Configuration" {        
+        & "$PWD\Configurations\MetaConfigPush.ps1" -TargetClient $script:linuxClientName 
+        Set-DscLocalConfigurationManager -Path "$env:temp\MetaConfigPush"  -Verbose -CimSession $script:session
+        
+        $metaConfig = Get-DscLocalConfigurationManager -Verbose -CimSession $script:session
+        $metaConfig.RefreshMode | Should Be "Push" 
+        $metaConfig.ConfigurationModeFrequencyMins | Should Be "60"           
 	}
-
 	
-	It "Verify we can do Start DSC Configuration And Get Dsc Configuration" {
-           
-           & "$PWD\Configurations\FileProviderTestConfig1.ps1" -targetClient $script:linuxClientName -Ensure "Present"
-           Start-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession $script:session -wait 
-           
-           #Verify Get Dsc 
-           $dscConfig = Get-DscConfiguration -Verbose -CimSession $script:session
-           $dscConfig.Ensure | Should Be "Present" 
-           $dscConfig.DestinationPath | Should Be "/tmp/dsctest1" 
-           $dscConfig.Contents | Should Be "Linux DSC Test1!" 
+	It "Verify we can do Start DSC Configuration And Get Dsc Configuration" {    
+        & "$PWD\Configurations\FileProviderTestConfig1.ps1" -TargetClient $script:linuxClientName -Ensure "Present"
+        Start-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession $script:session -Wait 
+        
+        #Verify Get Dsc 
+        $dscConfig = Get-DscConfiguration -Verbose -CimSession $script:session
+        $dscConfig.Ensure | Should Be "Present" 
+        $dscConfig.DestinationPath | Should Be "/tmp/dsctest1" 
+        $dscConfig.Contents | Should Be "Linux DSC Test1!" 
 
-           #Verify Test Dsc 
-           $testDscConfig = Test-DscConfiguration -CimSession $script:session -Detailed                      
-           $testDscConfig.InDesiredState | Should Be "True" 
+        #Verify Test Dsc 
+        $testDscConfig = Test-DscConfiguration -CimSession $script:session -Detailed                      
+        $testDscConfig.InDesiredState | Should Be "True" 
 
-
-           # Use Dsc to cleanup by deleting the file created above            
-            & "$PWD\Configurations\FileProviderTestConfig1.ps1" -targetClient $script:linuxClientName -Ensure "Absent"
-           Start-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession $script:session -wait            
-           
-           # Use Dsc to cleanup by deleting the file created above            
-            & "$PWD\Configurations\FileProviderTestConfig1.ps1" -targetClient $script:linuxClientName -Ensure "Absent"
-           Start-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession $script:session -wait            
-           $dscConfig = Get-DscConfiguration -Verbose -CimSession $script:session
-           $dscConfig.Ensure | Should Be "Absent" 
-           $dscConfig.DestinationPath | Should Be "/tmp/dsctest1"            
+        # Use Dsc to cleanup by deleting the file created above            
+        & "$PWD\Configurations\FileProviderTestConfig1.ps1" -TargetClient $script:linuxClientName -Ensure "Absent"
+        Start-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession $script:session -Wait            
+        
+        # Use Dsc to cleanup by deleting the file created above            
+        & "$PWD\Configurations\FileProviderTestConfig1.ps1" -TargetClient $script:linuxClientName -Ensure "Absent"
+        Start-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession $script:session -Wait            
+        $dscConfig = Get-DscConfiguration -Verbose -CimSession $script:session
+        $dscConfig.Ensure | Should Be "Absent" 
+        $dscConfig.DestinationPath | Should Be "/tmp/dsctest1"            
 	}
 	      
-
-    It "Verify we can do Publish and Start DSC -Useexisting " {
-            
-            & "$PWD\Configurations\FileProviderTestConfig1.ps1" -targetClient $script:linuxClientName -Ensure "Present"
-           Publish-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession $script:session                 
-           Start-DscConfiguration -UseExisting  -Verbose -CimSession $script:session -wait   
-           $dscConfig = Get-DscConfiguration -Verbose -CimSession $script:session
-           $dscConfig.Ensure | Should Be "Present" 
-           $dscConfig.DestinationPath | Should Be "/tmp/dsctest1"            
+    It "Verify we can do Publish and Start DSC -Useexisting" {
+        & "$PWD\Configurations\FileProviderTestConfig1.ps1" -TargetClient $script:linuxClientName -Ensure "Present"
+        Publish-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession $script:session                 
+        Start-DscConfiguration -UseExisting  -Verbose -CimSession $script:session -Wait   
+        $dscConfig = Get-DscConfiguration -Verbose -CimSession $script:session
+        $dscConfig.Ensure | Should Be "Present" 
+        $dscConfig.DestinationPath | Should Be "/tmp/dsctest1"            
 	}	
 
-    It "Verify we can do Update Dsc Configuration " {
-       
-           & "$PWD\Configurations\FileProviderTestConfig1.ps1" -targetClient $script:linuxClientName -Ensure "Absent"
-           Publish-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession            Update-DscConfiguration -wait -Verbose -CimSession $script:session 
-           $dscConfig = Get-DscConfiguration -Verbose -CimSession $script:session
-           $dscConfig.Ensure | Should Be "Absent" 
-           $dscConfig.DestinationPath | Should Be "/tmp/dsctest1"    
-	}	
-
+    It "Verify we can do Update Dsc Configuration" {
+        & "$PWD\Configurations\FileProviderTestConfig1.ps1" -TargetClient $script:linuxClientName -Ensure "Absent"
+        Publish-DscConfiguration -Path "$env:temp\FileProviderTestConfig1" -Verbose -CimSession
+        Update-DscConfiguration -Wait -Verbose -CimSession $script:session 
+        $dscConfig = Get-DscConfiguration -Verbose -CimSession $script:session
+        $dscConfig.Ensure | Should Be "Absent" 
+        $dscConfig.DestinationPath | Should Be "/tmp/dsctest1"    
+	}
 }
