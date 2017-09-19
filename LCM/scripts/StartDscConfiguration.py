26d2a35294bd75a89e07817128fbfc2f43070fbf
Katie Keim <kakeim@microsoft.com>
2017-09-19 13:14:13 -0700

    Add force parameter to StartDscConfiguration

--- LCM/scripts/StartDscConfiguration.py
+++ LCM/scripts/StartDscConfiguration.py
@@ -1,57 +1,119 @@
 #!/usr/bin/python
-import fileinput
-import sys
-import subprocess
 
-def usage():
-    print("Usage:")
-    print("    " + sys.argv[0] + " -configurationmof FILE")
-    sys.exit(1)
-    
-
-if len(sys.argv) != 3:
-    usage()
-
-if sys.argv[1].lower() != "-configurationmof":
-    usage()
+# Standard library imports
+from subprocess import Popen, PIPE
+from sys import argv
 
 try:
-    filedata = open(sys.argv[2], "r").read()
+    # Used by Python 2.7+
+    from argparse import ArgumentParser
+    useArgParse = True
 except:
-    filedata = open(sys.argv[2], "r", encoding="utf-16").read()
+    # Used by Python 2.4-2.6
+    from optparse import OptionParser
+    useArgParse = False
 
-outtokens = []
-for char in filedata:
-    outtokens.append(str(ord(char)))
+def main(argv):
+    """StartDscConfiguration"""
 
-omi_bindir = "<CONFIG_BINDIR>"
-omicli_path = omi_bindir + "/omicli"
+    # Define method arguments and description
+    description = 'Starts the specified DSC configuration.'
 
-parameters = []
-parameters.append(omicli_path)
-parameters.append("iv")
-parameters.append("<DSC_NAMESPACE>")
-parameters.append("{")
-parameters.append("MSFT_DSCLocalConfigurationManager")
-parameters.append("}")
-parameters.append("SendConfigurationApply")
-parameters.append("{")
-parameters.append("ConfigurationData")
-parameters.append("[")
-# insert configurationmof data here
-for token in outtokens:
-    parameters.append(token)
-parameters.append("]")
-parameters.append("}")
+    parameters = {
+        'configurationmof' : {
+            'shortForm' : 'c',
+            'helpText' : 'The path to the configuration mof to start.',
+            'required' : True,
+            'action' : 'store'
+        },
+        'force' : {
+            'shortForm' : 'f',
+            'helpText' : 'Specifies that any current pending configuration should be forcibly removed before starting the new configuration.',
+            'required' : False,
+            'action' : 'store_true'
+        }
+    }
 
-#s = ""
-#for param in parameters:
-#    s += param + " "
+    # Parse arguments
+    if (useArgParse):
+        # Used by Python 2.7+
+        parser = ArgumentParser(description = description)
 
-p = subprocess.Popen(parameters, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-stdout, stderr = p.communicate()
+        for parameter in parameters.keys():
+            parameterInfo = parameters[parameter]
+            parser.add_argument('-' + parameterInfo['shortForm'], '--' + parameter, required = parameterInfo['required'], help = parameterInfo['helpText'], action = parameterInfo['action'])
 
-print(stdout)
-print(stderr)
+        parsedArguments = parser.parse_args()
+    else:
+        # Used by Python 2.4-2.6
+        parser = OptionParser(description = description)
 
+        for parameter in parameters.keys():
+            parameterInfo = parameters[parameter]
+            parser.add_argument('-' + parameterInfo['shortForm'], '--' + parameter, help = parameterInfo['helpText'], action = parameterInfo['action'])
 
+        (parsedArguments, extraArguments) = parser.parse_args()
+
+        for parameter in parameters.keys():
+            if parameters[parameter]['required']:
+                if not parsedArguments[parameter]:
+                    print 'StartDscConfiguration.py: error: argument -' + parameters[parameter]['shortForm'] + '/--' + parameter + ' is required.'
+                    exit(1)
+
+    # Read the configuration mof
+    try:
+        configurationFile = open(parsedArguments.configurationmof, 'r')
+    except:
+        configurationFile = open(parsedArguments.configurationmof, 'r', encoding = 'utf-16')
+
+    try:
+        configurationFileContent = configurationFile.read()
+    finally:
+        configurationFile.close()
+
+    # Convert the file content to strings of integers representing unicode
+    configurationData = []
+    for char in configurationFileContent:
+        configurationData.append(str(ord(char)))
+
+    # OMI CLI location
+    omiBinDir = "<CONFIG_BINDIR>"
+    omiCliPath = omiBinDir + "/omicli"
+
+    # Assemble parameters to pass to OMI CLI
+    omiCliParameters = []
+    omiCliParameters.append(omiCliPath)
+    omiCliParameters.append("iv")
+    omiCliParameters.append("<DSC_NAMESPACE>")
+    omiCliParameters.append("{")
+    omiCliParameters.append("MSFT_DSCLocalConfigurationManager")
+    omiCliParameters.append("}")
+    omiCliParameters.append("SendConfigurationApply")
+    omiCliParameters.append("{")
+    omiCliParameters.append("ConfigurationData")
+    omiCliParameters.append("[")
+
+    # Insert configurationmof data here
+    for token in configurationData:
+        omiCliParameters.append(token)
+    
+    omiCliParameters.append("]")
+
+    # Insert force if specified
+    if parsedArguments.force:
+        omiCliParameters.append("force")
+        omiCliParameters.append("true")
+
+    omiCliParameters.append("}")
+
+    # Call OMI CLI in subprocess
+    omiCliProcess = Popen(omiCliParameters, stdout = PIPE, stderr = PIPE)
+
+    # Retrieve stdout and stderr from OMI CLI call
+    stdout, stderr = omiCliProcess.communicate()
+
+    # Print output from OMI CLI call
+    print(stdout)
+    print(stderr)
+
+main(argv[1:])
