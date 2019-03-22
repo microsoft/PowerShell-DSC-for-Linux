b2e54764196ff58e751d7db8d33733a810e22048
Zia Jalali <zijalali@microsoft.com>
2019-03-22 10:29:01 -0700

    set sighandler in lcmbusy

--- LCM/dsc/engine/ConfigurationManager/LocalConfigManagerHelper.c
+++ LCM/dsc/engine/ConfigurationManager/LocalConfigManagerHelper.c
@@ -6545,6 +6545,24 @@ MI_Result TimeToRunConsistencyCheck(
     return MI_RESULT_OK;
 }
 
+#if defined(BUILD_OMS)
+void handleSIGCHLDSignal(int sig)
+{
+    int saved_errorno = errno;
+
+    DSC_EventWriteMessageWaitForChildProcess();
+
+    // OMS providers registers the SIGINT handler but may not have
+    // an opportunity to clean up before getting unloaded.
+    // This code is to ensure that OMSConfig picks up the work left off by
+    // the OMS providers of cleaning up zombie processes.
+    // Only one instance of SIGCHLD can be queued, so it becomes necessary to reap
+    // several zombie processes during one invocation of the handler function.
+    while (waitpid((pid_t)(-1), 0, WNOHANG) > 0) { }
+    errno = saved_errorno;
+}
+#endif
+
 MI_Result SetLCMStatusBusy()
 {
         MI_Uint32 lcmStatus;
@@ -6552,6 +6570,15 @@ MI_Result SetLCMStatusBusy()
         MI_Result r;
         
         g_currentError[0] = '\0';
+
+#if defined(BUILD_OMS)
+        struct sigaction sa;
+        sa.sa_handler = &handleSIGCHLDSignal;
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+        sigaction(SIGCHLD, &sa, 0);
+        DSC_EventWriteMessageRegisterProcessHandler();
+#endif
 
         if (!g_LCMPendingReboot)
         {
@@ -6578,37 +6605,11 @@ MI_Result SetLCMStatusBusy()
         return r;
 }
 
-#if defined(BUILD_OMS)
-void handleSIGCHLDSignal(int sig)
-{
-    int saved_errorno = errno;
-
-    DSC_EventWriteMessageWaitForChildProcess();
-
-    // OMS providers registers the SIGINT handler but may not have
-    // an opportunity to clean up before getting unloaded.
-    // This code is to ensure that OMSConfig picks up the work left off by
-    // the OMS providers of cleaning up zombie processes.
-    // Only one instance of SIGCHLD can be queued, so it becomes necessary to reap
-    // several zombie processes during one invocation of the handler function.
-    while (waitpid((pid_t)(-1), 0, WNOHANG) > 0) { }
-    errno = saved_errorno;
-}
-#endif
-
 MI_Result SetLCMStatusReady()
 {
         MI_Uint32 lcmStatus;
         MI_Instance *extendedError;
         MI_Result r;
-#if defined(BUILD_OMS)
-        struct sigaction sa;
-        sa.sa_handler = &handleSIGCHLDSignal;
-        sigemptyset(&sa.sa_mask);
-        sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-        sigaction(SIGCHLD, &sa, 0);
-        DSC_EventWriteMessageRegisterProcessHandler();
-#endif
 
         if (!g_LCMPendingReboot)
         {
