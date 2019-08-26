1d5fb1bcb9ee3914277f690a16e62476b2c98a01
Mike Thompson <mithomps@microsoft.com>
2019-08-26 10:51:27 -0400

    task 4876044 - Add VMInsights module

---
+++ Providers/Modules/Plugins_UnitTests/VMInsights/plugin/Unit_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+require 'test/unit'
+
+require_relative 'mock_oms_test.rb'
+
+require_relative 'in_vminsights_test.rb'
+require_relative 'VMInsightsDataCollector_test.rb'
+require_relative 'VMInsightsEngine_test.rb'
