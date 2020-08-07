c50dd5e306ac8197208928e4cdbfb154cc85c699
Swapnil Jain <65277773+swjai@users.noreply.github.com>
2020-08-07 16:59:39 +0530

    priinting all updates in 2.4x

--- Providers/Scripts/2.6x-2.7x/Scripts/nxAvailableUpdates.py
+++ Providers/Scripts/2.6x-2.7x/Scripts/nxAvailableUpdates.py
@@ -134,7 +134,7 @@ def GetYumUpdates(Name):
     LG().Log('DEBUG', "Retrieving update package list using cmd: " + cmd)
     retcode, pkg_list = RunGetOutput(cmd, False, False)
     LG().Log('DEBUG', "Cmd execution time: " + str((time.time() - start_time) / 60))
-    #LG().Log('DEBUG', "Cmd output for update list : " + pkg_list)    
+    LG().Log('DEBUG', "Cmd output for update list : " + pkg_list)    
 
     # Sample output from OMSYumUpdates.sh
     #   ca-certificates.noarch
