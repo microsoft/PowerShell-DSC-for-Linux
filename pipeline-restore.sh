9d6bc1fc2982cab8f35ff641a840b97eb0a17c6e
Jagpreet Singh Tamber <jtamber@microsoft.com>
2021-11-17 19:41:12 +0530

    UPdate

--- pipeline-restore.sh
+++ pipeline-restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo yum install make -y
-sudo yum install bison -y
-sudo yum install rpm -y
-sudo yum install curl -y
-sudo yum install openssl -y
-sudo yum install wget -y
+# sudo yum install make -y
+# sudo yum install bison -y
+# sudo yum install rpm -y
+# sudo yum install curl -y
+# sudo yum install openssl -y
+# sudo yum install wget -y
