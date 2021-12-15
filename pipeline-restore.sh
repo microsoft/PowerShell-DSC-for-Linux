67671c105abfb1858e1c5a779827087121ce40fc
Jagpreet Singh Tamber <jtamber@microsoft.com>
2021-12-15 10:32:21 +0530

    update

--- pipeline-restore.sh
+++ pipeline-restore.sh
@@ -9,7 +9,7 @@ sudo apt-get install rpm -y
 sudo apt-get install librpm-dev -y
 sudo apt-get install libpam0g-dev -y
 sudo apt-get install libssl-dev -y
-#sudo apt-get install libmysqlclient-dev -y
+sudo apt-get install libmysqlclient-dev -y
 sudo apt-get install curl -y
 sudo apt-get install libcurl4-gnutls-dev -y
 sudo apt-get install zlib1g -y
@@ -19,6 +19,5 @@ sudo apt-get install pkg-config -y
 sudo apt-get install libkrb5-dev -y
 sudo apt-get install libtry-tiny-perl -y
 
-cd /tmp
-wget https://www.python.org/ftp/python/2.4.6/Python-2.4.6.tgz
-wget https://sourceforge.net/projects/ctypes/files/ctypes/1.0.2/ctypes-1.0.2.tar.gz
+wget -q https://www.python.org/ftp/python/2.4.6/Python-2.4.6.tgz -P /tmp
+wget -q https://sourceforge.net/projects/ctypes/files/ctypes/1.0.2/ctypes-1.0.2.tar.gz -P /tmp
