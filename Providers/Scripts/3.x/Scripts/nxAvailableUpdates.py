9558bcd429bfe835b01ba905563669f2fe18a551
nikhim-um <77010473+nikhim-um@users.noreply.github.com>
2022-05-26 14:52:27 +0530

    Change in Classification from "Security" to "Security Updates" (#853)
    Initially, Added classification name as "Security" which as being converted anyway as "Security Updates" in ruby file. This change is to send correct name from the assessment code itself.

--- Providers/Scripts/3.x/Scripts/nxAvailableUpdates.py
+++ Providers/Scripts/3.x/Scripts/nxAvailableUpdates.py
@@ -105,7 +105,7 @@ def GetAptUpdates(Name):
         d['Architecture'] = pkg[3]
         d['Version'] = pkg[1]
         if d['Name'] in security_patch_list:
-            d['Classification'] = "Security"
+            d['Classification'] = "Security Updates"
         else:
             d['Classification'] = "Others"
         d['Repository'] = pkg[2]
@@ -281,7 +281,7 @@ def get_yum_updates_list(yum_pkg_info_list, security_updates, Name):
             d['Version'] = '0:' + d['Version']
         d['Version'] = d['Version'].replace('(none)', '0')  # Handle the Epoch '(none)'.
         if str(d['Name']).strip() + "." + str(d['Architecture']).strip() in security_updates:
-           d['Classification'] = "Security"
+           d['Classification'] = "Security Updates"
         else:
            d['Classification'] = "Others"
         d['Repository'] = yum_pkg_info.group(5)
@@ -351,7 +351,7 @@ def GetZypperUpdates(Name):
             continue
         d['Architecture'] = pkg[5].strip()
         if d['Name'] in packages_from_patch_data:
-           d['Classification'] = "Security"
+           d['Classification'] = "Security Updates"
         else:
            d['Classification'] = "Others"
         d['Version'] = "0:" + pkg[4].strip()
