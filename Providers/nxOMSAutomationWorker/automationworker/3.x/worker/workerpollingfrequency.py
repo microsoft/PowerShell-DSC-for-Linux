629536dac3a5b8d7d0a8af567c7193a54a123375
adishijha <adishijha@microsoft.com>
2022-06-29 15:51:19 +0530

    updates

--- Providers/nxOMSAutomationWorker/automationworker/3.x/worker/workerpollingfrequency.py
+++ Providers/nxOMSAutomationWorker/automationworker/3.x/worker/workerpollingfrequency.py
@@ -6,9 +6,16 @@
 """Contains functions to get polling frequency value and set values based on the header received as part of GetSandboxActions """
 
 JRDS_SANDBOX_POLLING_FREQUENCY = ['30']
+DEFAULT_JRDS_SANDBOX_POLLING_FREQUENCY = '30'
 
 def set_jrds_sandbox_actions_polling_freq(pollingfrequency):
-    JRDS_SANDBOX_POLLING_FREQUENCY[0] = pollingfrequency
+    if type(pollingfrequency) != str:
+        try:
+            JRDS_SANDBOX_POLLING_FREQUENCY[0] = str(pollingfrequency)
+        except:
+            JRDS_SANDBOX_POLLING_FREQUENCY[0] = DEFAULT_JRDS_SANDBOX_POLLING_FREQUENCY
+    else:
+        JRDS_SANDBOX_POLLING_FREQUENCY[0] = pollingfrequency
 
 
 def get_jrds_get_sandbox_actions_polling_freq():
