ea69f6f1662fb3924f766e6ed451609430a4a388
Kunal Karmakar <kkarmakar@microsoft.com>
2022-08-16 10:49:10 +0530

    Revert back changes in file belonging to UM

--- Providers/nxAvailableUpdates/MSFT_nxAvailableUpdatesResource.cpp
+++ Providers/nxAvailableUpdates/MSFT_nxAvailableUpdatesResource.cpp
@@ -228,11 +228,7 @@ void MI_CALL MSFT_nxAvailableUpdatesResource_Invoke_InventoryTargetResource(
 		FILE *fp = NULL;
 		
 		clientBuffer = (MI_Uint8*)malloc(clientBufferLength + 1);
-        if(clientBuffer == NULL) {
-            return;
-        }
-        memset(&application, 0, sizeof(MI_Application));
-		MI_Application_Initialize(0,NULL,NULL, &application);
+        MI_Application_Initialize(0,NULL,NULL, &application);
 		result = XmlSerializer_Create(&application, 0, "MI_XML", &serializer);
 		if (result != MI_RESULT_OK)
 		{
@@ -251,10 +247,7 @@ void MI_CALL MSFT_nxAvailableUpdatesResource_Invoke_InventoryTargetResource(
 			// Try again with a buffer given to us by the clientBufferNeeded field
 			clientBufferLength = clientBufferNeeded;
 			clientBuffer = (MI_Uint8*)malloc(clientBufferLength + 1);
-            if(clientBuffer == NULL) {
-                return;
-            }
-			result = XmlSerializer_SerializeInstance( &serializer, 0, retInstance, clientBuffer, clientBufferLength, &clientBufferNeeded);
+            result = XmlSerializer_SerializeInstance( &serializer, 0, retInstance, clientBuffer, clientBufferLength, &clientBufferNeeded);
 		    }
 		    else
 		    {
@@ -270,16 +263,13 @@ void MI_CALL MSFT_nxAvailableUpdatesResource_Invoke_InventoryTargetResource(
 		if (result == MI_RESULT_OK)
 		{
 		    clientBuffer[clientBufferNeeded] = '\0';
-		    printf("%s", (char*)clientBuffer);
+		    printf((char*)clientBuffer);
 		}
 
 		
 		{
-		    char * reportTemplate = (char*)malloc(strlen(reportTemplateBase) + 1);
-            if(reportTemplate == NULL) {
-                return;
-            }
-		    strcpy(reportTemplate, reportTemplateBase);
+		    char * reportTemplate = (char*)malloc(strlen(reportTemplateBase));
+            strcpy(reportTemplate, reportTemplateBase);
 		    int fd = mkstemp(reportTemplate);
 		    if (fd == -1)
 		    {
